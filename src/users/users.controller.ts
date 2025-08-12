0ec333143d27d9891e8a6a99828be0bbbc168b00
gabrielfurigo <199030@upf.br>
2025-08-11 22:04:46 -0300

    first commit

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,16 +3,18 @@ import {
     ParseIntPipe
 } from '@nestjs/common';
 import { CreateUserDto } from 'src/dtos/create-user-dtos';
+import { UsersRepository } from 'src/users/repository/users.repository';
+
 @Controller('api/users')
 export class UsersController {
-    constructor(
-    ) { }
+    constructor(private readonly usersRepository: UsersRepository) { }
     @Get()
     async getAllUsers() {
         return {}
     }
     @Post()
     async createUser(@Body() body: CreateUserDto) {
+        await this.usersRepository.createUser(body);
     }
     @Put(':id')
     async updateUser(@Param('id', ParseIntPipe) id: number,
